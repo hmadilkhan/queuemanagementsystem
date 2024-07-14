c623302f6dc4b58c8c72a388f65cb73ac8b91e89
hmadilkhan <adilsziu@outlook.com>
2024-07-14 23:46:15 +0500

    Company & Branch Start

---
+++ app/Livewire/Branch/Delete.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Branch;
+
+use Livewire\Component;
+
+class Delete extends Component
+{
+    public function render()
+    {
+        return view('livewire.branch.delete');
+    }
+}
---
+++ app/Livewire/Company/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Company;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.company.index');
+    }
+}
